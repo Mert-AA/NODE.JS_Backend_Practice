7a51907d8ddc56418f3e4dc5ac99b9104a8c59cd
Mert-AA <mertataoglu2@gmail.com>
2026-07-12 21:33:43 +0300

    JS Practice

---
+++ Recalling_Javascript/Function.js
@@ -0,0 +1,16 @@
+var name = "Mert";
+var age = 28;
+var hasHobbies = true;
+
+function sumUser(name, age, hasHobbies) {
+  return (
+    "Name is " +
+    name +
+    "\nAge is " +
+    age +
+    "\nThe user has hobbies: " +
+    hasHobbies
+  );
+}
+
+console.log(sumUser(name, age, hasHobbies));
