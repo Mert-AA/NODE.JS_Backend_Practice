cc6104dfcaef7e6fceaa3c5105173acae4f232b3
Mert-AA <mertataoglu2@gmail.com>
2026-07-14 23:02:16 +0300

    Node.JS Creating Server Practice

--- NodeJS_Basics/app.js
+++ NodeJS_Basics/app.js
@@ -2,7 +2,11 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
-  res.end();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Mert's Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
   server.close();
 });
 
